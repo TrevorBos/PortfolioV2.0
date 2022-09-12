e273f6070cd193709cacf1cd319de97f4a8b255e
Trevor Bos <trevor.bos@outlook.com>
2022-09-12 19:49:03 -0400

    Adding in picture of myself and some brief about me text.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import HeroImage from "../assets/heroImage.jpg";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-purple-900">
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row ">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">I am a Full Stack Developer!</h2>
+          <p classname="text-white py-4 max-w-md">
+            I have almost one year of experiece creating and designing software.
+            Currently, I get the most passionate about using specific
+            technologies such as React, Tailwind, NodeJs, and MYSQL.
+          </p>
+          <div>
+            <button classname="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-pink-500 to-pink-800">
+              Portfolio
+              <span>
+                <MdOutlineKeyboardArrowRight />
+              </span>
+            </button>
+          </div>
+        </div>
+        <img src={HeroImage} alt="My profile" className="rounded-2xl mx-auto w-2/3 md:w-full" />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -51,7 +51,7 @@ const NavBar = () => {
       </div>
 
       {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-purple-800 text-gray-500">
           {links.map(({ id, link }) => (
             <li
               key={id}
