44b52c7964027523fd7669769b5d832fa57a9522
Trevor Bos <trevor.bos@outlook.com>
2022-09-13 18:28:23 -0400

    Replaced resume with new one.

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "resume.pdf",
+      href: "TrevorBos_Resume2022.pdf",
       style: "rounded-br-md",
       download: true,
     },
